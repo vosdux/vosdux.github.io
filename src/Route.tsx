b8ca15a016ab3ddac8e4060fa6fe6be054472fbc
Artem <karpuxov.a@mail.ru>
2022-12-22 20:50:05 +0300

    fix routes

--- src/Route.tsx
+++ src/Route.tsx
@@ -7,7 +7,7 @@ const LoginPage = React.lazy(() => import('./pages/LoginPage'));
 const SignUpPage = React.lazy(() => import('./pages/SignUpPage'));
 
 export const AppRoute = () => {
-  const BASENAME = process.env.NODE_ENV === 'development' ? '/' : '/react-18-typescript'; // because of github pages
+  const BASENAME = process.env.NODE_ENV === 'development' ? '/' : '/webstack-platform'; // because of github pages
 
   return (
     <BrowserRouter basename={BASENAME}>
