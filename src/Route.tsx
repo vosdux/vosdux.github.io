78cc712e1407bc608098cb7573e0db583ab83a0d
Artem <45297517+vosdux@users.noreply.github.com>
2022-12-22 23:57:44 +0300

    Update Route.tsx

--- src/Route.tsx
+++ src/Route.tsx
@@ -7,10 +7,8 @@ const LoginPage = React.lazy(() => import('./pages/LoginPage'));
 const SignUpPage = React.lazy(() => import('./pages/SignUpPage'));
 
 export const AppRoute = () => {
-  const BASENAME = process.env.NODE_ENV === 'development' ? '/' : '/webstack-platform'; // because of github pages
-
   return (
-    <BrowserRouter basename={BASENAME}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
