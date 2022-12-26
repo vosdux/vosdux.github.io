5f2c9dc2efcb2f16e659e621fba58a361e10b1e0
Akexander Dorofyuchik <derafwork@gmail.com>
2022-12-26 11:09:49 +0300

    feat: register page

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -1,7 +1,139 @@
 import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { root, container__login, register__input, login__block, register__checkbox } from './styles';
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
 
-const SignUp = () => {
-  return <div>sign up</div>;
+const SignUp: React.FC = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values: string) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <div className={root}>
+      <div className={container__login}>
+        <Form
+          {...formItemLayout}
+          form={form}
+          name="register"
+          onFinish={onFinish}
+          initialValues={{
+            residence: ['zhejiang', 'hangzhou', 'xihu'],
+            prefix: '86',
+          }}
+          scrollToFirstError
+          className={login__block}
+        >
+          <h2>Регистрация</h2>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                type: 'email',
+                message: 'Введен неверный E-mail!',
+              },
+              {
+                required: true,
+                message: 'Пожалуйста, введите свой E-mail!',
+              },
+            ]}
+          >
+            <Input className={register__input} prefix={<UserOutlined />} placeholder="E-mail" />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, введите ваш пароль!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Пароль"
+              className={register__input}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="confirm"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, подтвердите свой пароль!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                },
+              }),
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Подтвердите пароль"
+              className={register__input}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="agreement"
+            valuePropName="checked"
+            className={register__checkbox}
+            rules={[
+              {
+                validator: (_, value) =>
+                  value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+              },
+            ]}
+            {...tailFormItemLayout}
+          >
+            <Checkbox>
+              Я прочитал <a href="">соглашение</a>
+            </Checkbox>
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">
+              Зарегистрироваться
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
