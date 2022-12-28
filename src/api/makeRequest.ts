fb41f163cd9cfb5b2dafb148bb72c0dd498f075f
Artem <karpuxov.a@mail.ru>
2022-12-29 02:04:52 +0300

    feat: connect to api init

--- src/api/makeRequest.ts
+++ src/api/makeRequest.ts
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export const makeRequest = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
+  headers: { authorization: `Bearer ${localStorage.getItem('token')}` },
   withCredentials: true,
 });
