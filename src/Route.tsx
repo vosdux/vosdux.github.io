27f95ec2499857894a0bbab8ce64a6a9ce02ebd8
Akexander Dorofyuchik <derafwork@gmail.com>
2023-01-03 09:17:53 +0300

    feat: changePassword

--- src/Route.tsx
+++ src/Route.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from '@pages/LandingPage';
 import LoginPage from '@pages/LoginPage';
 import SignUpPage from '@pages/SignUpPage';
+import ChangePassword from '@pages/ChangePassword';
 
 // const LandingPage = React.lazy(() => import('./pages/LandingPage'));
 const DashboardPage = React.lazy(() => import('./pages/DashboardPage'));
@@ -17,6 +18,7 @@ export const AppRoute = () => {
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignUpPage />} />
+        <Route path="/change-password/:link" element={<ChangePassword />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/ChangePassword/index.tsx
@@ -0,0 +1,119 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, Form, Input } from 'antd';
+import { LockOutlined } from '@ant-design/icons';
+import {
+  root,
+  container__сhangePassword,
+  сhangePassword__input,
+  сhangePassword__block,
+  сhangePassword__btn,
+} from './styles';
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 },
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 },
+  },
+};
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 16,
+      offset: 8,
+    },
+  },
+};
+
+const ChangePassword: React.FC = () => {
+  const navigate = useNavigate();
+  const [form] = Form.useForm();
+
+  const onFinish = (values: string) => {
+    console.log('Received values of form: ', values);
+  };
+
+  return (
+    <div className={root}>
+      <div className={container__сhangePassword}>
+        <Form
+          {...formItemLayout}
+          form={form}
+          name="register"
+          onFinish={onFinish}
+          initialValues={{
+            residence: ['zhejiang', 'hangzhou', 'xihu'],
+            prefix: '86',
+          }}
+          scrollToFirstError
+          className={сhangePassword__block}
+        >
+          <h3>Введите новый пароль</h3>
+
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, введите ваш пароль!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Пароль"
+              className={сhangePassword__input}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="confirm"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Пожалуйста, подтвердите свой пароль!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('Введенный вами пароли не совпадают!'));
+                },
+              }),
+            ]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              type="password"
+              placeholder="Подтвердите пароль"
+              className={сhangePassword__input}
+            />
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout} className={сhangePassword__btn}>
+            <Button type="primary" htmlType="submit">
+              Изменить пороль
+            </Button>
+            <Button type="primary" onClick={() => navigate('/login')}>
+              Назад
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
+};
+
+export default ChangePassword;
