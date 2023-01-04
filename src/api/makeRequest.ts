9bb250da5541a35c4e587d896afc218d87032c6c
Akexander Dorofyuchik <derafwork@gmail.com>
2023-01-04 14:00:14 +0300

    feat: registration page

--- src/api/makeRequest.ts
+++ src/api/makeRequest.ts
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export const makeRequest = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
-  headers: { authorization: `Bearer ${localStorage.getItem('token')}`, 'Access-Control-Allow-Credentials': true },
-
+  headers: { authorization: `Bearer ${localStorage.getItem('token')}` },
   withCredentials: true,
 });
